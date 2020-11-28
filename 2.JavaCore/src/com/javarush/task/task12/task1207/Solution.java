e834c064b4e6a045f62602840f57c391de9608b1
fateunicorn <fedorsaenko@protonmail.ch>
2020-11-28 14:00:43 +0300

    у

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        Integer i = new Integer(1);
+        print(i);
+    }
+
+    public static void print(int i) {}
+    public static void print(Integer i) {}
+}
