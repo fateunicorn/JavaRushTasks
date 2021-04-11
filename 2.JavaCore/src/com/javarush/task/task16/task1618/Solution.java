80005dcbc235913b54ee8cbe14b382326d69a1ec
fateunicorn <fedorsaenko@protonmail.ch>
2021-04-11 23:15:12 +0300

    kk

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1618/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task16.task1618;
+
+/* 
+Снова interrupt
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        TestThread tt = new TestThread();
+        tt.start();
+        tt.interrupt();
+    }
+
+    //Add your code below - добавь код ниже
+    public static class TestThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            for (int i = 0; !super.isInterrupted(); i++) {
+
+            }
+        }
+    }
+}
